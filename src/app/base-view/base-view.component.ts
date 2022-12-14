ef0ad052b758d1fb1aa695bcefdb64688b4868e7
HomePC <grawires@gmail.com>
2022-12-14 12:52:07 +0100

    refactor

--- src/app/base-view/base-view.component.ts
+++ src/app/base-view/base-view.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NumbersRequest, NumbersResponse } from '../numbers-request';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-base-view',
@@ -8,55 +9,56 @@ import { NumbersRequest, NumbersResponse } from '../numbers-request';
   styleUrls: ['./base-view.component.css']
 })
 export class BaseViewComponent {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  apiUrl:string = "http://localhost:8080/numbers/sort-command"
-  testUrl:string = "http://localhost:8080/status/ping"
+  apiUrl: string = "http://localhost:8080/numbers/sort-command"
+  testUrl: string = "http://localhost:8080/status/ping"
 
-  pong:string = "";
-  pongCounter:number = 0;
+  pong: string = "";
+  pongCounter: number = 0;
 
-  numbersRequest:NumbersRequest = new NumbersRequest();
-  numbersResponse:NumbersResponse = new NumbersResponse();
-  orderByAscending:boolean = true;
-  numbersString:string = "";
-  responseString:string = "";
+  numbersRequest: NumbersRequest = new NumbersRequest();
+  numbersResponse: NumbersResponse = new NumbersResponse();
+  orderByAscending: boolean = true;
+  numbersString: string = "";
+  responseString: string = "";
 
-  ngOnInit(){
+  ngOnInit() {
 
   }
 
-  pingpong(){
-    return this.http.get(this.testUrl, { responseType: 'text' }).subscribe( data => {
+  pingpong() {
+    return this.http.get(this.testUrl, { responseType: 'text' }).subscribe(data => {
       console.log(data);
       this.pong = data;
       this.pongCounter++;
     });
   }
 
-  parseNumbersToArray(text:string){
-    let temp = text.split(",").map( e => { return parseInt(e); });
-    temp.forEach((element,index) => {
-      if(Number.isNaN(element) || element===null || element===undefined) temp.splice(index,1);
-    });
-    this.numbersRequest.numbers = temp;
+  parseNumbersToArray(text: string) {
+    let temp = text.split(",").map(e => { return parseInt(e); });
+    let filtered = temp.filter(el => Number.isInteger(el));
+
+    this.numbersRequest.numbers = filtered;
   }
 
-  submitNumbers(){
+  submitNumbers() {
     this.parseNumbersToArray(this.numbersString);
     console.warn(this.numbersRequest);
 
-    this.http.post<NumbersResponse>(this.apiUrl, this.numbersRequest).subscribe( response => {
-      this.responseString = "";
-      response.numbers.forEach(e => {
-        this.responseString += e.toString() + ",";
-      });      
-    });
+    this.http.post<NumbersResponse>(this.apiUrl, this.numbersRequest).subscribe(
+      response => {
+        this.responseString = "";
+        response.numbers.forEach(e => {
+          this.responseString += e.toString() + ",";
+        });
+      })
   }
 
-  onChangeOrder(){
-    if(this.orderByAscending) this.numbersRequest.order = "ASC"
+  onChangeOrder() {
+    if (this.orderByAscending) this.numbersRequest.order = "ASC"
     else this.numbersRequest.order = "DESC";
+    this.submitNumbers();
   }
 
 }
